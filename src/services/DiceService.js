5a6d6d079dbd411c6c8fbd43d0f6cb341f67c164
beanc16 <bean.christopher@outlook.com>
2023-04-29 00:14:29 -0400

    Update dice service business logic

--- src/services/DiceService.js
+++ src/services/DiceService.js
@@ -1,37 +1,54 @@
 const defaultParams = {
     sides: 10,
     count: 1,
+    rerollOnGreaterThanOrEqualTo: 10,
 };
 
 class DiceService
 {
-    roll({
+    constructor({
         sides = defaultParams.sides,
         count = defaultParams.count,
+        rerollOnGreaterThanOrEqualTo = defaultParams.rerollOnGreaterThanOrEqualTo,
     } = defaultParams)
+    {
+        this.sides = sides;
+        this.count = count;
+        this.rerollOnGreaterThanOrEqualTo = rerollOnGreaterThanOrEqualTo;
+    }
+
+    roll()
     {
         const rolls = [];
 
-        for (let i = 0; i < count; i++)
+        for (let i = 0; i < this.count; i++)
         {
-            const result = this.rollOne({ sides });
+            const result = this.rollOne();
             rolls.push(result);
         }
 
         return rolls;
     }
 
-    rollOne({
-        sides = defaultParams.sides,
-    } = defaultParams)
+    rollOne()
     {
+        const rolls = [];
+
         // Get a random integer between 1 and sides (inclusive)
-        const randomDecimal = Math.random() * sides;
+        const randomDecimal = Math.random() * this.sides;
         const randomInteger = Math.floor(randomDecimal) + 1;
-        return randomInteger;
+        rolls.push(randomInteger);
+
+        if (randomInteger >= this.rerollOnGreaterThanOrEqualTo)
+        {
+            const results = this.rollOne();
+            rolls.push(...results);
+        }
+
+        return rolls;
     }
 }
 
 
 
-module.exports = new DiceService();
+module.exports = DiceService;
