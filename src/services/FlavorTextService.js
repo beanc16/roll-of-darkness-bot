99d1f9a6f366c2d3e3a5ad32ae22fa228f772346
beanc16 <bean.christopher@outlook.com>
2023-04-30 17:53:03 -0400

    Update flavor text service retrieval

--- src/services/FlavorTextService.js
+++ src/services/FlavorTextService.js
@@ -1,4 +1,6 @@
 const { RollOfDarknessApi } = require('@beanc16/microservices-abstraction');
+const categoriesSingleton = require('../models/categoriesSingleton');
+const flavorTextSingleton = require('../models/flavorTextSingleton');
 
 class FlavorTextService
 {
@@ -7,8 +9,18 @@ class FlavorTextService
         this.rollOfDarknessApi = new RollOfDarknessApi();
     }
 
+    get categories()
+    {
+        return this._categories;
+    }
+
     async getCategories()
     {
+        if (categoriesSingleton.get()?.length > 0)
+        {
+            return categoriesSingleton.get();
+        }
+
         /*
          * Payload as of v1.8.1 of @beanc16/microservices-abstraction:
          *  {
@@ -29,7 +41,46 @@ class FlavorTextService
             return acc;
         }, {});
 
-        return parsedCategories;
+        categoriesSingleton.set(parsedCategories);
+        return categoriesSingleton.get();
+    }
+
+    async getFlavorText({
+        splat = categoriesSingleton.get('GENERAL'),
+        categories = [],
+    } = {})
+    {
+        if (!categoriesSingleton.isInitialized())
+        {
+            await this.getCategories();
+        }
+
+        // Get the cached flavor text if it exists
+        const flavorText = flavorTextSingleton.get({
+            keys: [
+                splat,
+                ...categories,
+            ],
+        });
+        if (flavorText?.length > 0)
+        {
+            return flavorText;
+        }
+
+        // Get the flavor text from the API
+        /*
+         * Payload as of v1.8.1 of @beanc16/microservices-abstraction:
+         *  {
+         *      "flavor_texts": [
+         *          "Flavor text 1.",
+         *          "Flavor text 2."
+         *      ]
+         *  }
+         */
+        return await this.rollOfDarknessApi.flavorText.get({
+            splat,
+            categories,
+        });
     }
 }
 
