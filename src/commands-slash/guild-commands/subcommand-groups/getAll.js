3f8199a514fd1aaacc856ff2d13d66f030cd240d
beanc16 <bean.christopher@outlook.com>
2023-05-07 13:26:37 -0400

    Add missing semicolon

--- src/commands-slash/guild-commands/subcommand-groups/getAll.js
+++ src/commands-slash/guild-commands/subcommand-groups/getAll.js
@@ -5,7 +5,7 @@ function getAll(subcommandGroup)
     subcommandGroup.setName('get_all');
     subcommandGroup.setDescription('Get all data from the database');
     subcommandGroup.addSubcommand(subcommands.getAllCategories);
-    subcommandGroup.addSubcommand(subcommands.getAllFlavorTexts)
+    subcommandGroup.addSubcommand(subcommands.getAllFlavorTexts);
     return subcommandGroup;
 }
 
