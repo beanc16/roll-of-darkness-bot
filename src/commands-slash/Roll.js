b5a3ef15dfc4887929a23ebf62a038cd07e5725f
beanc16 <bean.christopher@outlook.com>
2023-05-07 15:32:37 -0400

    Abstract base roll command behind superclass

--- src/commands-slash/Roll.js
+++ src/commands-slash/Roll.js
@@ -1,88 +1,13 @@
-const SplatSlashCommand = require('./base-commands/SplatSlashCommand');
+const BaseRollCommand = require('./base-commands/BaseRollCommand');
 const options = require('./options');
-const rollConstants = require('../constants/roll');
-const categoriesSingleton = require('../models/categoriesSingleton');
-const DiceService = require('../services/DiceService');
-const FlavorTextService = require('../services/FlavorTextService');
-const RollResponseFormatterService = require('../services/RollResponseFormatterService');
 
-class Roll extends SplatSlashCommand
+class Roll extends BaseRollCommand
 {
     constructor()
     {
-        super();
-        this._slashCommandData
-            .addIntegerOption(options.roll.numberOfDice)
-            .addStringOption(options.roll.rerolls)
-            .addBooleanOption(options.roll.rote)
-            .addIntegerOption(options.roll.exceptionalOn)
-            .addIntegerOption(options.roll.extraSuccesses)
-            .addBooleanOption(options.roll.advancedAction)
-            .addBooleanOption(options.roll.secret);
-    }
-
-    async run(interaction)
-    {
-        // Get initial parameter result
-        const isSecret = interaction.options.getBoolean('secret') || false;
-
-        // Send message to show the command was received
-        await interaction.deferReply({
-            ephemeral: isSecret,
-            fetchReply: true,
+        super({
+            firstParameter: options.roll.numberOfDice,
         });
-
-        // Get parameter results
-        const numberOfDice = interaction.options.getInteger('number_of_dice');
-        const splat = interaction.options.getString('splat') || categoriesSingleton.get('GENERAL');
-        const rerollsKey = interaction.options.getString('rerolls');
-        const isRote = interaction.options.getBoolean('rote');
-        const exceptionalOn = interaction.options.getInteger('exceptional_on');
-        const isAdvancedAction = interaction.options.getBoolean('advanced_action');
-        const extraSuccesses = interaction.options.getInteger('extra_successes');
-
-        // Convert parameters to necessary inputs for service calls
-        const rerollOnGreaterThanOrEqualTo = rollConstants.rerollsEnum[rerollsKey]?.number;
-        const rerollsDisplay = rollConstants.rerollsEnum[rerollsKey]?.display;
-
-        // Roll the dice
-        const diceService = new DiceService({
-            count: numberOfDice,
-            rerollOnGreaterThanOrEqualTo,
-            isRote,
-            isAdvancedAction,
-        });
-        const dicePoolGroup = diceService.roll();
-
-        // Flavor text
-        const flavorTextService = new FlavorTextService();
-        const flavorText = await flavorTextService.getRandomFlavorText({
-            splat,
-            categories: [
-                dicePoolGroup.getBiggestResult().successStatus,
-            ],
-        });
-
-        // Response
-        const rollResponseFormatterService = new RollResponseFormatterService({
-            authorId: interaction.user.id,
-            dicePoolGroup,
-            exceptionalOn,
-            extraSuccesses,
-            flavorText,
-            isAdvancedAction,
-            isRote,
-            numberOfDice,
-            rerollsDisplay,
-        });
-        await interaction.editReply(
-            rollResponseFormatterService.getResponse()
-        );
-    }
-
-    get description()
-    {
-        return `Roll d10s.`;
     }
 }
 
