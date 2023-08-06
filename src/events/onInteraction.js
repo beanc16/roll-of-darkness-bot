293856ae70d8cfed63008a4cd94320dd15e5d8c7
beanc16 <bean.christopher@outlook.com>
2023-08-06 17:50:43 -0400

    Update event to handler message context menus

--- src/events/onInteraction.js
+++ src/events/onInteraction.js
@@ -6,27 +6,50 @@ const SlashCommandsContainer = require('../slash-command-helpers/SlashCommandsCo
 
 async function handler(bot, interaction)
 {
-	if (!interaction.isChatInputCommand()) return;
+	if (interaction.isChatInputCommand())
+	{
+		const slashCommand = SlashCommandsContainer.getCommand(interaction.commandName)
+			|| SlashCommandsContainer.getGuildCommand(interaction.commandName);
+		if (!slashCommand)
+		{
+			logger.error(`No command named ${interaction.commandName} was found.`);
+			return;
+		}
 
-	const slashCommand = SlashCommandsContainer.getCommand(interaction.commandName)
-		|| SlashCommandsContainer.getGuildCommand(interaction.commandName);
-	if (!slashCommand)
-	{
-		logger.error(`No command named ${interaction.commandName} was found.`);
-		return;
+		try
+		{
+			await slashCommand.run(interaction);
+		}
+		catch (err)
+		{
+			logger.error(err);
+			await interaction.reply({
+				content: 'An error occurred while executing this command',
+				ephemeral: true,
+			});
+		}
 	}
+	else if (interaction.isMessageContextMenuCommand())
+	{
+		const contextMenuCommand = SlashCommandsContainer.getContextMenuCommand(interaction.commandName);
+		if (!contextMenuCommand)
+		{
+			logger.error(`No command named ${interaction.commandName} was found.`);
+			return;
+		}
 
-	try
-	{
-		await slashCommand.run(interaction);
-	}
-	catch (err)
-	{
-		logger.error(err);
-		await interaction.reply({
-			content: 'An error occurred while executing this command',
-			ephemeral: true,
-		});
+		try
+		{
+			await contextMenuCommand.run(bot, interaction);
+		}
+		catch (err)
+		{
+			logger.error(err);
+			await interaction.reply({
+				content: 'An error occurred while executing this command',
+				ephemeral: true,
+			});
+		}
 	}
 }
 
