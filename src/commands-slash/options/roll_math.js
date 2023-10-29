edeb1befd62fa7ea5d6197d3db1a72f877ec53f3
beanc16 <bean.christopher@outlook.com>
2023-10-29 17:24:11 -0400

    Update roll_math parameter description

--- src/commands-slash/options/roll_math.js
+++ src/commands-slash/options/roll_math.js
@@ -1,7 +1,7 @@
 function dicePool(option)
 {
     option.setName('dice_pool');
-    option.setDescription('A mathematical formula of dice to roll (only addition and subtraction are supported; IE: 4 + 3 - 2)');
+    option.setDescription('A mathematical formula of dice to roll (only addition and subtraction are supported; IE: 4+3-2)');
     option.setMinLength(1);
     option.setRequired(true);
     return option;
