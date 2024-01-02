13d780a199177014c5f2b51a21789a3352164d27
beanc16 <bean.christopher@outlook.com>
2024-01-01 20:50:28 -0500

    Create initial combat tracker command

---
+++ src/commands-slash/Combat_Tracker.ts
@@ -0,0 +1,69 @@
+import { BaseSlashCommand } from '@beanc16/discordjs-common-commands';
+import { CommandInteraction, ComponentType } from 'discord.js';
+
+import options from './options';
+import { getCombatTrackerEmbed as getCombatTrackerEmbedMessage } from './embed-messages/combat-tracker';
+import { getCombatTrackerActionRows } from './select-menus/combat_tracker';
+import { CombatTrackerType } from '../constants/combatTracker';
+import { logger } from '@beanc16/logger';
+
+class Combat_Tracker extends BaseSlashCommand
+{
+    constructor()
+    {
+        super();
+        this._slashCommandData
+            .addStringOption(options.combatTracker.type);
+    }
+
+    async run(interaction: CommandInteraction)
+    {
+        // Send message to show the command was received
+        const message = await interaction.deferReply({
+            fetchReply: true,
+        });
+
+        // Get parameter results
+        const typeKey = interaction.options.get('type')?.value as CombatTrackerType || CombatTrackerType.All;
+
+        // Get components
+        const actionRows = getCombatTrackerActionRows(typeKey);
+
+        message.awaitMessageComponent({
+            filter: (interaction) => (
+                interaction.componentType === ComponentType.StringSelect
+            ),
+            time: 60_000,
+        })
+        .then((interaction) => {
+            // TODO: Make this a separate function later, awaited, or something so it's separated.
+            console.log('\n interaction:', interaction);
+        })
+        .catch((error: Error) => 
+        {
+            // Ignore timeouts
+            if (error.message !== 'Collector received no interactions before ending with reason: time')
+            {
+                logger.error(error);
+            }
+        });
+
+        const embedMessage = getCombatTrackerEmbedMessage({
+            roundNumber: 1,
+            characters: [],
+        });
+
+        // Send response
+        await interaction.editReply({
+            embeds: [embedMessage],
+            components: actionRows,
+        });
+    }
+
+    get description()
+    {
+        return `Track characters' HP and/or initiative in combat.`;
+    }
+}
+
+export = new Combat_Tracker();
