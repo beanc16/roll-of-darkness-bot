16a6e35636eeaaeacbb02d010594dd5a4b6bdc42
beanc16 <bean.christopher@outlook.com>
2024-02-10 18:07:00 -0500

    Save current hp service progress

--- src/services/WorldOfDarknessHpService.ts
+++ src/services/WorldOfDarknessHpService.ts
@@ -23,6 +23,15 @@ interface HpSpecificParameters
     amount: number;
 }
 
+interface UpgradeParameters
+{
+    maxHp: number;
+    bashingDamage: number;
+    lethalDamage: number;
+    aggravatedDamage: number;
+    damageType: 'bashing' | 'lethal' | 'agg';
+}
+
 export class WorldOfDarknessHpService
 {
     static #healSpecificDamage({
@@ -163,15 +172,85 @@ export class WorldOfDarknessHpService
         return output;
     }
 
-    static #upgrade({
-        maxHp,
+    static #dealSpecificDamage({
         bashingDamage,
         lethalDamage,
         aggravatedDamage,
         amount,
-        damageType,
-    }: HpParameters)
+    }: HpSpecificParameters): HpResponse
     {
+        const orderedDamage = [
+            aggravatedDamage,
+            lethalDamage,
+            bashingDamage,
+        ];
+
+        // Heal each damage in order
+        const {
+            damage: [
+                newAggravatedDamage,
+                newLethalDamage,
+                newBashingDamage,
+            ],
+        } = orderedDamage.reduce((acc, damage) =>
+        {
+            if (acc.amount <= 0 || damage <= 0)
+            {
+                acc.damage.push(damage);
+                return acc;
+            }
+
+            const remainingDamage = damage + acc.amount;
+            acc.damage.push(Math.max(remainingDamage, 0));
+            acc.amount = (remainingDamage > 0)
+                ? 0                     // Has damage of this tier left, so stop healing
+                : remainingDamage * -1; // Overflowed healing, so should do damage to lower tiers of damage types
+
+            return acc;
+        }, {
+            damage: [] as number[],
+            amount,
+        });
+
+        return {
+            newAggravatedDamage,
+            newLethalDamage,
+            newBashingDamage,
+        };
+    }
+
+    static #upgradeDamage({
+        maxHp,
+        bashingDamage,
+        lethalDamage,
+        aggravatedDamage,
+        damageType,
+    }: UpgradeParameters)
+    {
+        const output = {
+            newBashingDamage: bashingDamage,
+            newLethalDamage: lethalDamage,
+            newAggravatedDamage: aggravatedDamage,
+        };
+        let damageToUpgrade = bashingDamage + lethalDamage + aggravatedDamage - maxHp;
+
+        if (damageToUpgrade <= -1)
+        {
+            return output;
+        }
+
+        damageToUpgrade += 1;
+
+        console.log('output 1:', output);
+        console.log('damageToUpgrade:', damageToUpgrade);
+
+        if (damageType === 'bashing' && bashingDamage > 0)
+        {
+            output.newBashingDamage = bashingDamage - damageToUpgrade - 1;
+            output.newLethalDamage = lethalDamage + damageToUpgrade;
+        }
+
+        return output;
     }
 
     static damage({
@@ -183,42 +262,48 @@ export class WorldOfDarknessHpService
         damageType,
     }: HpParameters)
     {
-        // Case 1: {
-        //     maxHp: 10, // 0/10 damage tracks have damage in them
-        //     aggravatedDamage: 0,
-        //     lethalDamage: 0,
-        //     bashingDamage: 0,
-        //     amount: 3,
-        //     damageType: 'bashing',
-        // }
-        // Case 2: {
-        //     maxHp: 10, // 6/10 damage tracks have damage in them
-        //     aggravatedDamage: 1,
-        //     lethalDamage: 2,
-        //     bashingDamage: 3,
-        //     amount: 3,
-        //     damageType: 'bashing',
-        // }
-        // Case 3: {
-        //     maxHp: 10, // 9/10 damage tracks have damage in them
-        //     aggravatedDamage: 4,
-        //     lethalDamage: 3,
-        //     bashingDamage: 2,
-        //     amount: 3,
-        //     damageType: 'bashing',
-        // }
-        const emptyHealthBoxes = Math.max(maxHp - aggravatedDamage - lethalDamage - bashingDamage, 0);
-
-        const output = {
-            newAggravatedDamage: aggravatedDamage,
-            newLethalDamage: lethalDamage,
-            newBashingDamage: bashingDamage,
+        const input = {
+            // Default to not healing all damage types (by setting them to 0)
+            bashingDamage: 0,
+            lethalDamage: 0,
+            aggravatedDamage: 0,
+            amount,
         };
 
-        if (damageType === 'bashing')
+        if (damageType === 'agg')
         {
+            input.bashingDamage = bashingDamage;
+            input.lethalDamage = lethalDamage;
+            input.aggravatedDamage = aggravatedDamage;
+        }
+        else if (damageType === 'lethal')
+        {
+            input.bashingDamage = bashingDamage;
+            input.lethalDamage = lethalDamage;
+        }
+        else if (damageType === 'bashing')
+        {
+            input.bashingDamage = bashingDamage;
         }
 
-        return output;
+        const output = this.#dealSpecificDamage(input);
+
+        if (damageType === 'lethal')
+        {
+            output.newAggravatedDamage = aggravatedDamage;
+        }
+        else if (damageType === 'bashing')
+        {
+            output.newAggravatedDamage = aggravatedDamage;
+            output.newLethalDamage = lethalDamage;
+        }
+
+        return this.#upgradeDamage({
+            maxHp,
+            bashingDamage: output.newBashingDamage,
+            lethalDamage: output.newLethalDamage,
+            aggravatedDamage: output.newAggravatedDamage,
+            damageType,
+        });
     }
 }
