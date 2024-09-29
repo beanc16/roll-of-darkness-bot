1e6455b46dd43712be574f408c21f00932a8cf16
beanc16 <bean.christopher@outlook.com>
2024-09-28 20:14:46 -0400

    Add power and weight chart quick reference

--- src/commands-slash/options/subcommand-groups/ptu/quickReference.ts
+++ src/commands-slash/options/subcommand-groups/ptu/quickReference.ts
@@ -5,8 +5,10 @@ export enum PtuQuickReferenceSubcommand
     DamageCharts = 'damage_charts',
     NatureChart = 'nature_chart',
     PokemonExperienceChart = 'pokemon_experience_chart',
+    PowerChart = 'power_chart',
     SwitchingPokemon = 'switching_pokemon',
     TrainingPokemon = 'training_pokemon',
+    WeightClassChart = 'weight_class_chart',
     TypeChart = 'type_chart',
 }
 
@@ -34,6 +36,14 @@ export const pokemonExperienceChart = (subcommand: SlashCommandSubcommandBuilder
     return subcommand;
 };
 
+export const powerChart = (subcommand: SlashCommandSubcommandBuilder) =>
+{
+    subcommand.setName(PtuQuickReferenceSubcommand.PowerChart);
+    subcommand.setDescription(`Get a chart showing weight limits for power.`);
+
+    return subcommand;
+};
+
 export const switchingPokemon = (subcommand: SlashCommandSubcommandBuilder) =>
 {
     subcommand.setName(PtuQuickReferenceSubcommand.SwitchingPokemon);
@@ -46,6 +56,14 @@ export const trainingPokemon = (subcommand: SlashCommandSubcommandBuilder) =>
 {
     subcommand.setName(PtuQuickReferenceSubcommand.TrainingPokemon);
     subcommand.setDescription('Get info about how to train your pokemon.');
+
+    return subcommand;
+};
+
+export const weightClassChart = (subcommand: SlashCommandSubcommandBuilder) =>
+{
+    subcommand.setName(PtuQuickReferenceSubcommand.WeightClassChart);
+    subcommand.setDescription(`Get a chart showing weight ranges for each weight class.`);
 
     return subcommand;
 };
--- src/commands-slash/strategies/ptu/quickReference/index.ts
+++ src/commands-slash/strategies/ptu/quickReference/index.ts
@@ -1,15 +1,19 @@
 import { QrDamageChartsStrategy } from './QrDamageChartsStrategy.js';
 import { QrNatureChartStrategy } from './QrNatureChartStrategy.js';
 import { QrPokemonExperienceChartStrategy } from './QrPokemonExperienceChartStrategy.js';
+import { QrPowerChartStrategy } from './QrPowerChartStrategy.js';
 import { QrSwitchingPokemonStrategy } from './QrSwitchingPokemonStrategy.js';
 import { QrTrainingPokemonStrategy } from './QrTrainingPokemonStrategy.js';
 import { QrTypeChartStrategy } from './QrTypeChartStrategy.js';
+import { QrWeightClassChartStrategy } from './QrWeightClassChartStrategy.js';
 
 export default {
     [QrDamageChartsStrategy.key]: QrDamageChartsStrategy,
     [QrNatureChartStrategy.key]: QrNatureChartStrategy,
     [QrPokemonExperienceChartStrategy.key]: QrPokemonExperienceChartStrategy,
+    [QrPowerChartStrategy.key]: QrPowerChartStrategy,
     [QrSwitchingPokemonStrategy.key]: QrSwitchingPokemonStrategy,
     [QrTrainingPokemonStrategy.key]: QrTrainingPokemonStrategy,
+    [QrWeightClassChartStrategy.key]: QrWeightClassChartStrategy,
     [QrTypeChartStrategy.key]: QrTypeChartStrategy,
 };
