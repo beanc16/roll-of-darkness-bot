c9f3dd8d9021688db50219b0bcd18cf246b4e517
beanc16 <bean.christopher@outlook.com>
2024-10-12 15:14:37 -0400

    Make use of retry logic for google sheets service & add better error handling

--- src/services/CachedGoogleSheetsApiService.ts
+++ src/services/CachedGoogleSheetsApiService.ts
@@ -8,16 +8,28 @@ import {
 import { CachedAuthTokenService } from './CachedAuthTokenService.js';
 import { logger } from '@beanc16/logger';
 import { CompositeKeyRecord } from './CompositeKeyRecord.js';
+import { Timer } from './Timer.js';
+
+export enum GoogleSheetsApiErrorType {
+    UserNotAddedToSheet = 'AUTOMATED_USER_NOT_ADDED_TO_SHEET',
+    UnableToParseRange = 'UNABLE_TO_PARSE_RANGE',
+}
 
 interface GetRangeResponse
 {
     data?: string[][];
-    errorType?: 'AUTOMATED_USER_NOT_ADDED_TO_SHEET';
+    errorType?: GoogleSheetsApiErrorType;
+}
+
+interface GetRangesResponse
+{
+    data?: GoogleSheetsGetRangesResponseV1['data'];
+    errorType?: GoogleSheetsApiErrorType;
 }
 
 interface UpdateResponse
 {
-    errorType?: 'AUTOMATED_USER_NOT_ADDED_TO_SHEET';
+    errorType?: GoogleSheetsApiErrorType;
 }
 
 interface WithCacheOptions
@@ -27,7 +39,7 @@ interface WithCacheOptions
 
 export class CachedGoogleSheetsApiService
 {
-    static #retries = 1;
+    static #retries = 4;
     static #cache: CompositeKeyRecord<[string, string], string[][]> = new CompositeKeyRecord();
 
     static async getRange(initialParameters: GoogleSheetsGetRangeParametersV1 & WithCacheOptions): Promise<GetRangeResponse>
@@ -78,6 +90,15 @@ export class CachedGoogleSheetsApiService
                     continue;
                 }
 
+                else if (
+                    statusCode === 400
+                    && (error as { message?: string; }).message?.includes('Unable to parse range'))
+                {
+                    return {
+                        errorType: GoogleSheetsApiErrorType.UnableToParseRange,
+                    };
+                }
+
                 logger.error('An unknown error occurred on GoogleSheetsMicroservice.v1.getRange', { statusCode }, data, (error as any)?.response);
                 throw error;
             }
@@ -95,7 +116,7 @@ export class CachedGoogleSheetsApiService
                         }),
                     });
                     return {
-                        errorType: 'AUTOMATED_USER_NOT_ADDED_TO_SHEET',
+                        errorType: GoogleSheetsApiErrorType.UserNotAddedToSheet,
                     };
                 }
 
@@ -109,33 +130,72 @@ export class CachedGoogleSheetsApiService
         return {};
     }
 
-    static async getRanges(parameters: GoogleSheetsGetRangesParametersV1): Promise<GoogleSheetsGetRangesResponseV1['data'] | undefined>
+    static async getRanges(parameters: GoogleSheetsGetRangesParametersV1): Promise<GetRangesResponse>
     {
         for (let i = 0; i <= this.#retries; i += 1)
         {
             const authToken = await CachedAuthTokenService.getAuthToken();
-            const {
-                statusCode = 200,
-                data = [],
-                error,
-            } = await GoogleSheetsMicroservice.v1.getRanges(authToken, parameters);
 
-            if (statusCode === 200)
+            try
             {
-                return data;
+                const {
+                    statusCode = 200,
+                    data = [],
+                    error,
+                } = await GoogleSheetsMicroservice.v1.getRanges(authToken, parameters);
+
+                if (statusCode === 200)
+                {
+                    return { data };
+                }
+
+                else if (statusCode === 401)
+                {
+                    logger.info('A 401 error occurred on GoogleSheetsMicroservice.v1.getRanges. Retrieving new auth token.');
+                    await CachedAuthTokenService.resetAuthToken();
+                    continue;
+                }
+
+                throw error;
+            }
+            catch (error)
+            {
+                // Forbidden error (occurs when the robot user for google sheets isn't added or doesn't have perms on the sheet)
+                if ((error as any)?.response?.data?.statusCode === 403)
+                {
+                    logger.warn('The automated user is not added to the queried sheet. Please add them.', {
+                        ...(parameters?.ranges && {
+                            ranges: parameters?.ranges,
+                        }),
+                    });
+                    return {
+                        errorType: GoogleSheetsApiErrorType.UserNotAddedToSheet,
+                    };
+                }
+
+                else if (
+                    (error as any)?.response?.data?.statusCode === 400
+                    && (error as any)?.response?.data?.error?.message?.includes('Unable to parse range')
+                )
+                {
+                    return {
+                        errorType: GoogleSheetsApiErrorType.UnableToParseRange,
+                    };
+                }
+
+                else
+                {
+                    logger.error('An error occurred on GoogleSheetsMicroservice.v1.getRanges', (error as any)?.response?.data || error);
+                }
             }
 
-            else if (statusCode === 401)
-            {
-                logger.info('A 401 error occurred on GoogleSheetsMicroservice.v1.getRanges. Retrieving new auth token.');
-                await CachedAuthTokenService.resetAuthToken();
-                continue;
-            }
-
-            throw error;
+            // Wait half a second between retries
+            await Timer.wait({
+                seconds: 0.5,
+            });
         }
 
-        return undefined;
+        return {};
     }
 
     static async update(initialParameters: GoogleSheetsUpdateParametersV1 & WithCacheOptions): Promise<UpdateResponse>
@@ -195,7 +255,7 @@ export class CachedGoogleSheetsApiService
                         }),
                     });
                     return {
-                        errorType: 'AUTOMATED_USER_NOT_ADDED_TO_SHEET',
+                        errorType: GoogleSheetsApiErrorType.UserNotAddedToSheet,
                     };
                 }
 
