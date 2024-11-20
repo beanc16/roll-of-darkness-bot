9279f91221f158b235c2bc8ceba2da86bf0198bd
beanc16 <bean.christopher@outlook.com>
2024-11-20 13:31:05 -0500

    Create chunkArray function

---
+++ src/services/chunkArray.ts
@@ -0,0 +1,32 @@
+interface ChunkArrayOptions<Element>
+{
+    array: Element[];
+    shouldMoveToNextChunk: (item: Element, index: number, currentChunk: Element[]) => boolean;
+}
+
+export const chunkArray = <Element>({
+    array,
+    shouldMoveToNextChunk,
+}: ChunkArrayOptions<Element>) =>
+{
+    const output = array.reduce<Element[][]>((acc, cur, index) =>
+    {
+        const currentChunk = acc[acc.length - 1] ?? [];
+
+        // Create a new chunk
+        if (shouldMoveToNextChunk(cur, index, currentChunk) || acc.length === 0)
+        {
+            acc.push([cur]);
+        }
+
+        // Add to the current chunk
+        else
+        {
+            currentChunk.push(cur);
+        }
+
+        return acc;
+    }, []);
+
+    return output;
+};
