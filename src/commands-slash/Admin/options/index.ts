5efed2c29a44aa2f8029173bb7fb7a8b4e5cfda5
beanc16 <bean.christopher@outlook.com>
2024-12-04 23:25:00 -0500

    Filter out unnecessary autocomplete params from /admin refresh_cache

--- src/commands-slash/Admin/options/index.ts
+++ src/commands-slash/Admin/options/index.ts
@@ -14,12 +14,21 @@ export enum RefreshCacheCommand
     Ptu = 'ptu',
 }
 
+const ptuAutocompleteParameterNamesToIgnore = new Set([
+    PtuAutocompleteParameterName.PokemonName,
+    PtuAutocompleteParameterName.PokemonToEvolve,
+    PtuAutocompleteParameterName.Ability1,
+    PtuAutocompleteParameterName.Ability2,
+    PtuAutocompleteParameterName.Ability3,
+    PtuAutocompleteParameterName.Ability4,
+]);
+
 const nwodSubcommandsForRefreshCache = Object.values(NwodAutocompleteParameterName)
     .map(value => value);
 
 const ptuSubcommandsForRefreshCache = Object.values(PtuAutocompleteParameterName)
     .map(value => value)
-    .filter(value => value !== PtuAutocompleteParameterName.PokemonName);
+    .filter(value => !ptuAutocompleteParameterNamesToIgnore.has(value));
 
 export const commandsForRefresh = [
     ...nwodSubcommandsForRefreshCache.map<CommandForRefreshCache>(value =>
