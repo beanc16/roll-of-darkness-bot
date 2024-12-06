796a598e78e291d94f1a35e46f789995e7bcf9bb
beanc16 <bean.christopher@outlook.com>
2024-12-06 13:27:31 -0500

    Create joinWithOxfordComma helper function

--- src/services/stringHelpers.ts
+++ src/services/stringHelpers.ts
@@ -33,3 +33,20 @@ export function capitalizeFirstLetter(input: string): string
 {
     return input.charAt(0).toUpperCase() + input.slice(1);
 }
+
+export function joinWithOxfordComma(input: string[]): string
+{
+    if (input.length > 2)
+    {
+        const allButLast = input.slice(0, -1).join(', ');
+        const last = input[input.length - 1];
+        return `${allButLast}, and ${last}`;
+    }
+
+    if (input.length === 2)
+    {
+        return input.join(' and ');
+    }
+
+    return input.join(', ');
+}
---
+++ tests/services/stringHelpers/joinWithOxfordComma.test.ts
@@ -0,0 +1,56 @@
+import { joinWithOxfordComma } from '../../../src/services/stringHelpers.js';
+import { getFakeArray } from '../../fakes/arrays.js';
+
+describe('function: joinWithOxfordComma', () =>
+{
+    const value = 'value';
+
+    it('should return an empty string for an empty array', () =>
+    {
+        const input = getFakeArray(value, 0);
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual('');
+    });
+
+    it('should return a single string for an array with one element', () =>
+    {
+        const input = getFakeArray(value, 1);
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual('value');
+    });
+
+    it('should join an array with two elements with an "and"', () =>
+    {
+        const input = getFakeArray(value, 2);
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual('value and value');
+    });
+
+    it('should join an array with three elements with an Oxford comma', () =>
+    {
+        const input = getFakeArray(value, 3);
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual('value, value, and value');
+    });
+
+    it('should join an array with four elements with an Oxford comma', () =>
+    {
+        const input = getFakeArray(value, 4);
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual('value, value, value, and value');
+    });
+
+    it('should handle strings with commas correctly', () =>
+    {
+        const input = ['apple', 'orange, juicy', 'banana'];
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual('apple, orange, juicy, and banana');
+    });
+
+    it('should handle strings with leading or trailing spaces', () =>
+    {
+        const input = [' apple ', ' orange ', ' banana '];
+        const result = joinWithOxfordComma(input);
+        expect(result).toEqual(' apple ,  orange , and  banana ');
+    });
+});
