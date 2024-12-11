16755763b7cc4490dbee2106421462cf86303127
beanc16 <bean.christopher@outlook.com>
2024-12-11 12:00:35 -0500

    Add jest test thresholds for unit tests that're currently created

--- jest.config.cjs
+++ jest.config.cjs
@@ -74,6 +74,34 @@ module.exports = {
         '/.*/CachedRollOfDarknessApi.ts',
         '/.*/rollOfDarknessApiHasErrorSingleton.ts',
     ],
+    coverageThreshold: {
+        // TODO: Set up a global threshold when more unit tests are implemented
+        // TODO: Get all numbers up to 80-100% when more unit tests are implemented (depending on the location)
+        './src/commands-slash/embed-messages': {
+            statements: 50,
+            branches: 50,
+            functions: 50,
+            lines: 50,
+        },
+        './src/commands-slash/Nwod/strategies/calculate': {
+            statements: 50,
+            branches: 25,
+            functions: 60,
+            lines: 50,
+        },
+        './src/commands-slash/Ptu/models': {
+            statements: 90,
+            branches: 90,
+            functions: 100,
+            lines: 90,
+        },
+        './src/services/MathParser': {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
+    },
     moduleFileExtensions: ['js', 'ts'],
     extensionsToTreatAsEsm: ['.ts'],
     resolver: 'ts-jest-resolver',
