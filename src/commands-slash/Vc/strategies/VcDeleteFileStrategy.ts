b456cfcd51649ec34373a8f009f9f94d70b7323b
beanc16 <bean.christopher@outlook.com>
2025-02-28 17:46:54 -0500

    Update response if /vc delete_file is run for a file that does not exist

--- src/commands-slash/Vc/strategies/VcDeleteFileStrategy.ts
+++ src/commands-slash/Vc/strategies/VcDeleteFileStrategy.ts
@@ -1,9 +1,11 @@
+import { logger } from '@beanc16/logger';
 import { FileStorageMicroservice, FileStorageMicroserviceResourceType } from '@beanc16/microservices-abstraction';
 import { ChatInputCommandInteraction } from 'discord.js';
 
 import { staticImplements } from '../../../decorators/staticImplements.js';
 import { ChatIteractionStrategy } from '../../strategies/types/ChatIteractionStrategy.js';
 import { VcSubcommand } from '../options/index.js';
+import { VcViewFilesStrategy } from './VcViewFilesStrategy.js';
 
 @staticImplements<ChatIteractionStrategy>()
 export class VcDeleteFileStrategy
@@ -14,24 +16,56 @@ export class VcDeleteFileStrategy
     {
         const fileName = interaction.options.getString('file_name', true);
 
-        await this.deleteFile(interaction, fileName);
+        const success = await this.deleteFile(interaction, fileName);
 
-        await interaction.editReply({
-            content: `Deleted file: \`${fileName}\``,
-        });
+        if (success)
+        {
+            await interaction.editReply({
+                content: `Deleted file: \`${fileName}\``,
+            });
+        }
+        else
+        {
+            const fileNames = await VcViewFilesStrategy.getFileNamesMessage(interaction);
+            await interaction.editReply({
+                content: `Failed to delete file: \`${fileName}\`. ${fileNames}`,
+            });
+        }
 
         return true;
     }
 
-    private static async deleteFile(interaction: ChatInputCommandInteraction, fileName: string): Promise<void>
+    private static async deleteFile(
+        interaction: ChatInputCommandInteraction,
+        fileName: string,
+    ): Promise<boolean>
     {
-        await FileStorageMicroservice.v1.delete({
-            app: {
-                id: process.env.APP_ID as string,
-            },
-            fileName,
-            nestedFolders: `vc-commands/${interaction.user.id}`,
-            resourceType: FileStorageMicroserviceResourceType.Audio,
-        });
+        try
+        {
+            await FileStorageMicroservice.v1.delete({
+                app: {
+                    id: process.env.APP_ID as string,
+                },
+                fileName,
+                nestedFolders: `vc-commands/${interaction.user.id}`,
+                resourceType: FileStorageMicroserviceResourceType.Audio,
+            });
+            return true;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        catch (err: any)
+        {
+            // The file does not exist
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            if ((err?.response?.data?.error?.message || '') === 'Request failed with status code 404')
+            {
+                return false;
+            }
+
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            logger.warn('An unknown error occurred while deleting an audio file', err?.response?.data ?? err);
+        }
+
+        return false;
     }
 }
