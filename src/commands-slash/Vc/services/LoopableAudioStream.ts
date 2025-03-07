74f5b42247b2aeb5bc762a795059eed94bd69128
beanc16 <bean.christopher@outlook.com>
2025-03-07 18:47:51 -0500

    Update code formatting in LoopableAudioStream

--- src/commands-slash/Vc/services/LoopableAudioStream.ts
+++ src/commands-slash/Vc/services/LoopableAudioStream.ts
@@ -1,6 +1,6 @@
 import { Readable } from 'node:stream';
 
-// This allows us to loop an audio Buffer without
+// This class allows us to loop an audio Buffer without
 // any extra discord.js event-handling logic.
 
 export class LoopableAudioStream extends Readable
@@ -19,21 +19,27 @@ export class LoopableAudioStream extends Readable
     // eslint-disable-next-line no-underscore-dangle
     public _read(size: number): void
     {
-        const chunk = Uint8Array.prototype.slice.call(this.buffer, this.position, this.position + size);
+        const chunk = Uint8Array.prototype.slice.call(
+            this.buffer,
+            this.position,
+            this.position + size,
+        );
         this.push(chunk);
 
         this.position += chunk.length;
 
         if (this.position >= this.buffer.length)
         {
+            // Restart from the beginning
             if (this.shouldLoop)
             {
-                this.position = 0; // Restart from the beginning
+                this.position = 0;
             }
 
+            // End the stream
             else
             {
-                this.push(null); // End the stream
+                this.push(null);
             }
         }
     }
