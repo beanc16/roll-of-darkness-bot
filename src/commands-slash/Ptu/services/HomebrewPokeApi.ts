89d30fff86096da2bdd4e9a069ccbfcec344956b
beanc16 <bean.christopher@outlook.com>
2025-03-30 23:04:07 -0400

    Update unknown image url handling for eden pokemon

--- src/commands-slash/Ptu/services/HomebrewPokeApi.ts
+++ src/commands-slash/Ptu/services/HomebrewPokeApi.ts
@@ -6,31 +6,61 @@ interface HomebrewGetImageUrlResponse
     imageUrl: string;
 }
 
+let unknownImageUrl: string | undefined;
+
+try
+{
+    const { data } = await FileStorageMicroservice.v1.get({
+        appId: process.env.APP_ID as string,
+        fileName: 'Unknown',
+        nestedFolders: 'ptu-pokedex/eden-dex',
+    });
+
+    unknownImageUrl = data.url;
+}
+catch (error)
+{
+    // No-op
+}
+
 // TODO: Add unit tests
 
 export class HomebrewPokeApi
 {
     public static async getImageUrls(names?: string[]): Promise<HomebrewGetImageUrlResponse[] | undefined>
     {
-        const promises = names?.map(name => FileStorageMicroservice.v1.get({
+        if (names === undefined || names.length === 0)
+        {
+            return undefined;
+        }
+
+        const promises = names.map(name => FileStorageMicroservice.v1.get({
             appId: process.env.APP_ID as string,
             fileName: name,
             nestedFolders: 'ptu-pokedex/eden-dex',
         }));
 
-        if (!promises || promises.length === 0)
-        {
-            return undefined;
-        }
+        const results = await Promise.allSettled(promises);
 
-        const results = await Promise.all(promises);
-
-        const output = results.reduce<HomebrewGetImageUrlResponse[]>((acc, { data }) =>
+        const output = results.reduce<HomebrewGetImageUrlResponse[]>((acc, cur, index) =>
         {
-            if (data)
+            // Image could not be found, therefore use unknown image url
+            if (cur.status === 'rejected' && unknownImageUrl !== undefined)
             {
-                const { url } = data;
-                const pokemonName = names?.find(name => url.toLowerCase().includes(name.toLowerCase()));
+                acc.push({ name: names[index], imageUrl: unknownImageUrl });
+                return acc;
+            }
+
+            // Image could be found
+            if (cur.status === 'fulfilled' && cur.value.data)
+            {
+                const {
+                    value: {
+                        data: { url },
+                    },
+                } = cur;
+                const urlWithoutSpecialCharacters = url.replaceAll(/%20/g, ' ').replaceAll(/%28/g, '(').replaceAll(/%29/g, ')');
+                const pokemonName = names.find(name => urlWithoutSpecialCharacters.toLowerCase().includes(name.toLowerCase()));
 
                 if (pokemonName)
                 {
