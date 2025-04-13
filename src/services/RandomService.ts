f4249a92e2633b80b99e30e8b2671ac9579b4720
beanc16 <bean.christopher@outlook.com>
2025-04-13 15:31:35 -0400

    Create Randomservice.getUniqueRandomIntegers

--- src/services/RandomService.ts
+++ src/services/RandomService.ts
@@ -16,4 +16,31 @@ export class RandomService
         const randomDecimal = this.getRandomDecimal(max);
         return Math.floor(randomDecimal) + 1;
     }
+
+    /**
+     * Get an array of unique integers between 1 and max (inclusive)
+     */
+    public static getUniqueRandomIntegers(max: number, count: number): number[]
+    {
+        if (count > max)
+        {
+            throw new Error('Count must be less than or equal to max');
+        }
+
+        // Generate an array of integers from 1 to max (inclusive)
+        const integers = Array.from({ length: max }, (_, index) => index + 1);
+
+        const output: number[] = [];
+        for (let loopIndex = 0; loopIndex < count; loopIndex += 1)
+        {
+            // Get a random index
+            const indexToRemove = this.getRandomInteger(max - loopIndex);
+
+            // Remove the random result from the array
+            const [result] = integers.splice(indexToRemove - 1, 1);
+            output.push(result);
+        }
+
+        return output;
+    }
 }
--- test/services/RandomService.test.ts
+++ test/services/RandomService.test.ts
@@ -39,4 +39,41 @@ describe('class: RandomService', () =>
             });
         });
     });
+
+    describe('method: getUniqueRandomIntegers', () =>
+    {
+        it.each([
+            ['with max of 1000 and count of 100', {
+                max: 1000,
+                count: 100,
+            }],
+            ['with max of 100 and count of 1000', {
+                max: 100,
+                count: 100,
+            }],
+        ])('should return an array of unique integers %s', (_, { max, count }) =>
+        {
+            const results = RandomService.getUniqueRandomIntegers(max, count);
+
+            // Returns the correct number of results
+            expect(results.length).toEqual(count);
+
+            // The results are unique
+            expect(new Set(results).size).toEqual(count);
+
+            // The results are in the given range
+            results.forEach((result) =>
+            {
+                expect(result).toBeGreaterThanOrEqual(1);
+                expect(result).toBeLessThanOrEqual(max);
+            });
+        });
+
+        it('should throw an error if count is greater than max', () =>
+        {
+            const max = 10;
+            const count = 11;
+            expect(() => RandomService.getUniqueRandomIntegers(max, count)).toThrow();
+        });
+    });
 });
