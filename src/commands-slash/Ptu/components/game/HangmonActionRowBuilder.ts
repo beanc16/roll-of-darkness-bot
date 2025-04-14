e54d5521dab3bdb37f42d2acb5a0b994ad644d54
beanc16 <bean.christopher@outlook.com>
2025-04-13 20:35:32 -0400

    Create HangmonActionRowBuilder

---
+++ src/commands-slash/Ptu/components/game/HangmonActionRowBuilder.ts
@@ -0,0 +1,79 @@
+import {
+    ActionRowBuilder,
+    type Message,
+    type StringSelectMenuInteraction,
+} from 'discord.js';
+
+import { CommandName } from '../../../../types/discord.js';
+import { PaginatedStringSelectMenu } from '../../../components/PaginatedStringSelectMenu.js';
+import { PtuPokemonForLookupPokemon } from '../../embed-messages/lookup.js';
+import { HangmonState } from '../../types/hangmon.js';
+import { HangmonEmbedMessage } from './HangmonEmbedMessage.js';
+
+interface HangmonActionRowBuilderOptions
+{
+    message: Message;
+    commandName: CommandName;
+    embed: HangmonEmbedMessage;
+    state: HangmonState;
+    onSelect: (
+        receivedInteraction: StringSelectMenuInteraction,
+        actionRowBuilder: HangmonActionRowBuilder,
+    ) => Promise<void>;
+}
+
+export class HangmonActionRowBuilder extends ActionRowBuilder<PaginatedStringSelectMenu<PtuPokemonForLookupPokemon>>
+{
+    private stringSelectMenu: PaginatedStringSelectMenu<PtuPokemonForLookupPokemon>;
+
+    constructor({
+        message,
+        commandName,
+        embed,
+        state,
+        onSelect,
+    }: HangmonActionRowBuilderOptions)
+    {
+        const stringSelectMenu = new PaginatedStringSelectMenu({
+            customId: 'hangmon_selection',
+            elementName: 'Pokemon',
+            elements: state.remainingPokemonOptions,
+            message,
+            commandName,
+            embeds: [embed],
+            onSelect: (receivedInteraction) => onSelect(receivedInteraction, this),
+            optionParser: (curPokemon) =>
+            {
+                const typesLabel = (curPokemon.types.length > 1)
+                    ? 'Types'
+                    : 'Type';
+
+                return {
+                    label: curPokemon.name,
+                    value: curPokemon.name,
+                    description: [
+                        `${typesLabel}: ${curPokemon.types.join(' / ')}`,
+                        `${curPokemon.metadata.source}`,
+                    ].join(' | '),
+                };
+            },
+        });
+
+        super({
+            components: [
+                stringSelectMenu,
+                // TODO: Add guess history button
+            ],
+        });
+
+        this.stringSelectMenu = stringSelectMenu;
+    }
+
+    public update({ embed, state }: Pick<HangmonActionRowBuilderOptions, 'embed' | 'state'>): void
+    {
+        this.stringSelectMenu.update({
+            embeds: [embed],
+            elements: state.remainingPokemonOptions,
+        });
+    }
+}
