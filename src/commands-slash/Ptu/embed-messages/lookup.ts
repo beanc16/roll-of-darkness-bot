c64493b41a7d91f9b071871bc5368fab66fb80f1
beanc16 <bean.christopher@outlook.com>
2025-05-11 13:57:32 -0400

    Add fallback for pokemon lookup if pokemon contains no egg groups in its egg group array

--- src/commands-slash/Ptu/embed-messages/lookup.ts
+++ src/commands-slash/Ptu/embed-messages/lookup.ts
@@ -204,7 +204,7 @@ export const getLookupPokemonEmbedMessages = (
             '',
             Text.bold('Breeding Information'),
             `Gender Ratio: ${!genderRatio.none ? `${genderRatio.male}% M / ${genderRatio.female}% F` : 'No Gender'}`,
-            `Egg Group${eggGroups.length > 1 ? 's' : ''}: ${eggGroups.join(' / ')}`,
+            ...(eggGroups.length > 0 ? [`Egg Group${eggGroups.length > 1 ? 's' : ''}: ${eggGroups.join(' / ')}`] : ['None']),
             ...(averageHatchRate ? [`Average Hatch Rate: ${averageHatchRate}`, ''] : ['']),
             Text.bold('Environment'),
             `Diet: ${diets.join(', ')}`,
