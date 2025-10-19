6396481dadeab4727ecb6199f3614716f3805129
beanc16 <bean.christopher@outlook.com>
2025-10-18 23:14:11 -0400

    Create PtuSkillRankService w/ full unit test coverage

---
+++ src/commands-slash/Ptu/types/types.ts
@@ -0,0 +1,10 @@
+export enum PtuSkillRank
+{
+    Pathetic = 'Pathetic',
+    Untrained = 'Untrained',
+    Novice = 'Novice',
+    Adept = 'Adept',
+    Expert = 'Expert',
+    Master = 'Master',
+    Virtuoso = 'Virtuoso',
+}
