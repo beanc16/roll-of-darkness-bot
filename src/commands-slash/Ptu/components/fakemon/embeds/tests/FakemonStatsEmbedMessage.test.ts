a79947c1ecf6d49be1171f3bff425c6a867e8c86
beanc16 <bean.christopher@outlook.com>
2025-12-20 23:34:16 -0500

    Add FakemonStatsEmbedMessage unit tests

---
+++ src/commands-slash/Ptu/components/fakemon/embeds/tests/FakemonStatsEmbedMessage.test.ts
@@ -0,0 +1,41 @@
+import { FakemonStatsEmbedMessage } from '../FakemonStatsEmbedMessage';
+
+describe('class: FakemonStatsEmbedMessage', () =>
+{
+    const input: ConstructorParameters<typeof FakemonStatsEmbedMessage>[0] = {
+        baseStats: {
+            hp: 1,
+            attack: 2,
+            defense: 3,
+            specialAttack: 4,
+            specialDefense: 5,
+            speed: 6,
+        },
+    };
+
+    describe('constructor', () =>
+    {
+        it('should construct', () =>
+        {
+            const embedMessage = new FakemonStatsEmbedMessage(input);
+            expect(embedMessage).toBeDefined();
+        });
+    });
+
+    describe('method: constructDescriptionLines', () =>
+    {
+        it('should construct description lines', () =>
+        {
+            const descriptionLines = FakemonStatsEmbedMessage.constructDescriptionLines(input);
+            expect(descriptionLines).toEqual([
+                'HP: 1',
+                'Attack: 2',
+                'Defense: 3',
+                'Special Attack: 4',
+                'Special Defense: 5',
+                'Speed: 6',
+                'Total: 21',
+            ]);
+        });
+    });
+});
