6191a3151355a4b7e0966cc5549f21f8f6f597c5
beanc16 <bean.christopher@outlook.com>
2025-12-29 00:22:52 -0500

    Add FakemonBasicInformationManagerService.setAbilities

--- src/commands-slash/Ptu/services/FakemonDataManagers/FakemonBasicInformationManagerService.ts
+++ src/commands-slash/Ptu/services/FakemonDataManagers/FakemonBasicInformationManagerService.ts
@@ -31,4 +31,76 @@ export class FakemonBasicInformationManagerService
             types,
         });
     }
+
+    public static async setAbilities({
+        messageId,
+        fakemon,
+        abilities,
+    }: {
+        messageId: string;
+        fakemon: PtuFakemonCollection;
+        abilities: Partial<PtuFakemonCollection['abilities']>;
+    }): Promise<PtuFakemonCollection>
+    {
+        const shouldUpdate: Record<keyof PtuFakemonCollection['abilities'], boolean> = {
+            basicAbilities: !!abilities.basicAbilities && abilities.basicAbilities?.length > 0,
+            advancedAbilities: !!abilities.advancedAbilities && abilities.advancedAbilities?.length > 0,
+            highAbility: !!abilities.highAbility && abilities.highAbility?.length > 0,
+        };
+
+        // Exit early if there's no abilities to update
+        if (Object.values(abilities).length === 0)
+        {
+            return fakemon;
+        }
+
+        const abilitiesToValidate: PtuFakemonCollection['abilities'] = {
+            basicAbilities: abilities.basicAbilities ?? fakemon.abilities.basicAbilities,
+            advancedAbilities: abilities.advancedAbilities ?? fakemon.abilities.advancedAbilities,
+            highAbility: abilities.highAbility ?? fakemon.abilities.highAbility,
+        };
+
+        if (
+            abilitiesToValidate.basicAbilities.length === 0
+            || abilitiesToValidate.basicAbilities.length > 2
+        )
+        {
+            throw new Error('Fakemon must have 1-2 basic abilities');
+        }
+        if (
+            abilitiesToValidate.advancedAbilities.length === 0
+            || abilitiesToValidate.advancedAbilities.length > 3
+        )
+        {
+            throw new Error('Fakemon must have 1-3 advanced abilities');
+        }
+        if (!abilities.highAbility)
+        {
+            throw new Error('Fakemon must have a high ability');
+        }
+        if (!((
+            abilitiesToValidate.basicAbilities.length === 2
+            && abilitiesToValidate.advancedAbilities.length === 2
+        ) || (
+            abilitiesToValidate.basicAbilities.length === 1
+            && abilitiesToValidate.advancedAbilities.length === 3
+        ))) {
+            throw new Error('Fakemon must have 2 basic abilities and 2 advanced abilities OR 1 basic ability and 3 advanced abilities');
+        }
+
+        return await PtuFakemonPseudoCache.update(messageId, { id: fakemon.id }, {
+            abilities: {
+                ...fakemon.abilities,
+                ...(shouldUpdate.basicAbilities && {
+                    basicAbilities: abilitiesToValidate.basicAbilities,
+                }),
+                ...(shouldUpdate.advancedAbilities && {
+                    advancedAbilities: abilitiesToValidate.advancedAbilities,
+                }),
+                ...(shouldUpdate.highAbility && {
+                    highAbility: abilitiesToValidate.highAbility,
+                }),
+            },
+        });
+    }
 }
--- src/commands-slash/Ptu/services/FakemonDataManagers/tests/FakemonBasicInformationManagerService.test.ts
+++ src/commands-slash/Ptu/services/FakemonDataManagers/tests/FakemonBasicInformationManagerService.test.ts
@@ -127,4 +127,195 @@ describe(`class: ${FakemonBasicInformationManagerService.name}`, () =>
             expect(updateSpy).not.toHaveBeenCalled();
         });
     });
+
+    describe(`method: ${FakemonBasicInformationManagerService.setAbilities.name}`, () =>
+    {
+        it(`should update the fakemon's abilities`, async () =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const expectedResult = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update')
+                .mockResolvedValue(expectedResult);
+            const args: Parameters<typeof FakemonBasicInformationManagerService.setAbilities>[0] = {
+                messageId,
+                fakemon,
+                abilities: {
+                    basicAbilities: ['Ability 1', 'Ability 2'],
+                    advancedAbilities: ['Ability 3', 'Ability 4'],
+                    highAbility: 'Ability 5',
+                },
+            };
+
+            // Act
+            const result = await FakemonBasicInformationManagerService.setAbilities(args);
+
+            // Assert
+            expect(result).toEqual(expectedResult);
+            expect(updateSpy).toHaveBeenCalledWith(
+                messageId,
+                { id: fakemon.id },
+                { abilities: args.abilities },
+            );
+        });
+
+        it('should not update if no abilities are provided', async () =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {},
+                }),
+            ).resolves.not.toThrow();
+            expect(updateSpy).not.toHaveBeenCalled();
+        });
+
+        it('should not throw an error if 2 basic abilities, 2 advanced abilities, & 1 high ability are provided', async () =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        basicAbilities: ['Ability 1', 'Ability 2'],
+                        advancedAbilities: ['Ability 3', 'Ability 4'],
+                        highAbility: 'Ability 5',
+                    },
+                }),
+            ).resolves.not.toThrow();
+            expect(updateSpy).toHaveBeenCalled();
+        });
+
+        it('should not throw an error if 1 basic ability, 3 advanced abilities, & 1 high ability are provided', async () =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        basicAbilities: ['Ability 1'],
+                        advancedAbilities: ['Ability 2', 'Ability 3', 'Ability 4'],
+                        highAbility: 'Ability 5',
+                    },
+                }),
+            ).resolves.not.toThrow();
+            expect(updateSpy).toHaveBeenCalled();
+        });
+
+        it.each([0, 3, 4, 5])('should throw an error if provided %s basic abilities (AKA: Not 1-2)', async (numOfAbilities) =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        basicAbilities: Array.from({ length: numOfAbilities }, (_, index) => `Ability ${index}`),
+                    },
+                }),
+            ).rejects.toThrow('Fakemon must have 1-2 basic abilities');
+            expect(updateSpy).not.toHaveBeenCalled();
+        });
+
+        it.each([0, 4, 5, 6])('should throw an error if provided %s advanced abilities (AKA: Not 1-3)', async (numOfAbilities) =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        advancedAbilities: Array.from({ length: numOfAbilities }, (_, index) => `Ability ${index}`),
+                    },
+                }),
+            ).rejects.toThrow('Fakemon must have 1-3 advanced abilities');
+            expect(updateSpy).not.toHaveBeenCalled();
+        });
+
+        it('should throw an error if provided no high ability (AKA: Not 1)', async () =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        advancedAbilities: Array.from({ length: 2 }, (_, index) => `Ability ${index}`),
+                    },
+                }),
+            ).rejects.toThrow('Fakemon must have a high ability');
+            expect(updateSpy).not.toHaveBeenCalled();
+        });
+
+        it('should throw an error if not provided 2 basic abilities, 2 advanced abilities, & 1 high ability OR not provided 1 basic ability, 3 advanced abilities, & 1 high ability', async () =>
+        {
+            // Arrange
+            const messageId = 'messageId';
+            const fakemon = createPtuFakemonCollectionData();
+            const updateSpy = jest.spyOn(PtuFakemonPseudoCache, 'update');
+
+            // Act & Assert - 2 basic abilities, "2" advanced abilities, & 1 high ability
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        basicAbilities: Array.from({ length: 2 }, (_, index) => `Ability ${index}`),
+                        advancedAbilities: Array.from({ length: 3 }, (_, index) => `Ability ${index}`),
+                        highAbility: 'Ability 5',
+                    },
+                }),
+            ).rejects.toThrow('Fakemon must have 2 basic abilities and 2 advanced abilities OR 1 basic ability and 3 advanced abilities');
+            expect(updateSpy).not.toHaveBeenCalled();
+
+            // Act & Assert - 1 basic ability, "3" advanced abilities, & 1 high ability
+            await expect(
+                FakemonBasicInformationManagerService.setAbilities({
+                    messageId,
+                    fakemon,
+                    abilities: {
+                        basicAbilities: Array.from({ length: 1 }, (_, index) => `Ability ${index}`),
+                        advancedAbilities: Array.from({ length: 2 }, (_, index) => `Ability ${index}`),
+                        highAbility: 'Ability 5',
+                    },
+                }),
+            ).rejects.toThrow('Fakemon must have 2 basic abilities and 2 advanced abilities OR 1 basic ability and 3 advanced abilities');
+            expect(updateSpy).not.toHaveBeenCalled();
+        });
+    });
 });
